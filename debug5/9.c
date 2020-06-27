c23409b5f2b4528b01ac49ea9dd0f2dfabd45461
fengshen0417 <67299057+fengshen0417@users.noreply.github.com>
2020-06-27 20:32:36 +0800

    debug
    debug

---
+++ debug5/9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    int i, j, k;
+    char name[10][20] = { 0 };
+    for (i = 0; i < 10; i++) {
+        scanf("%s", name[i]);
+    }
+
+    for (i = 0, j = 0; i < 10; i++)
+    {
+        for (k = 0; k < 9 - j; k++)
+        {
+            if (strcmp(&name[k][j], &name[k + 1][j]) > 0)
+            {
+                char temp[21];
+                strcpy(temp, &name[k][j]);
+                strcpy(&name[k][j], &name[k + 1][j]);
+                strcpy(&name[k + 1][j], &temp);
+            }
+        }
+    }
+
+    for (i = 0, j = 0; i < 10; i++) {
+        printf("%s\n", &name[i][j]);
+    }
+
+    return 0;
+}
